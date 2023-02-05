f14c7ecedb41e7e62608e821080bc524d4a796c6
Denis Mihaylov <116539046+deniskmihaylov@users.noreply.github.com>
2023-02-05 20:42:34 +0200

    Add files via upload

---
+++ addAndSubtract.js
@@ -0,0 +1,25 @@
+function subtract (arr) {
+
+    let oldSum = 0; 
+    let newSum = 0; 
+
+    for (let i = 0; i < arr.length; i++) {
+
+        let currentNum = arr[i]; 
+        oldSum += currentNum; 
+
+        if (currentNum % 2 === 0) {
+            currentNum += i; 
+            arr[i] = currentNum
+        } else {
+            currentNum -= i; 
+            arr[i] = currentNum; 
+        }
+        newSum += currentNum; 
+    }
+    console.log(arr); 
+    console.log(oldSum); 
+    console.log(newSum)
+}
+
+subtract([5, 15, 23, 56, 35])
